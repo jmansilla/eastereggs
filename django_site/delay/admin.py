da16e3d5b0d0c5df9140e366a1e9289614a68ac9
Javier Mansilla <javimansilla@gmail.com>
2024-08-07 11:54:00 -0300

    created Deadline model, and basic migrations for the 2 needed deadlines: show and print

--- django_site/delay/admin.py
+++ django_site/delay/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import SOGroup, PingPong
+from .models import SOGroup, PingPong, Deadline
+
+
+class DeadlineAdmin(admin.ModelAdmin):
+    list_display = ('name', 'deadline')
 
 admin.site.register(SOGroup)
 admin.site.register(PingPong)
+admin.site.register(Deadline, DeadlineAdmin)
