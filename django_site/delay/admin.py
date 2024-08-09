9d53d16aa2d65b985dccafc52139ed58dac66428
Javier Mansilla <javimansilla@gmail.com>
2024-08-08 21:52:44 -0300

    renamed some fields (group_number to repo_name, secret_key to password_to_win, beaten to challenge_won, and beaten_timestamp to challenge_won_timestamp. Added validation to repo_name field, added auto-genaration to password_to_win field. Added help-text to each field of SoGroup

--- django_site/delay/admin.py
+++ django_site/delay/admin.py
@@ -6,6 +6,9 @@ from .models import SOGroup, PingPong, Deadline
 class DeadlineAdmin(admin.ModelAdmin):
     list_display = ('name', 'deadline')
 
-admin.site.register(SOGroup)
+class SOGroupAdmin(admin.ModelAdmin):
+    readonly_fields=('password_to_win', 'challenge_won', 'challenge_won_timestamp', 'wasted_time')
+
+admin.site.register(SOGroup, SOGroupAdmin)
 admin.site.register(PingPong)
 admin.site.register(Deadline, DeadlineAdmin)
