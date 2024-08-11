d27d463a2c87c09c25fe81c49c53a8c05bc4b02b
Javier Mansilla <javimansilla@gmail.com>
2024-08-11 17:11:26 -0300

    displaying wasted minutes

--- django_site/delay/admin.py
+++ django_site/delay/admin.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from django.contrib import admin
 
 from .models import SOGroup, PingPong, Deadline
@@ -8,7 +10,12 @@ class DeadlineAdmin(admin.ModelAdmin):
 
 class SOGroupAdmin(admin.ModelAdmin):
     readonly_fields=('password_to_win', 'challenge_won', 'challenge_won_timestamp', 'wasted_time')
-    list_display = ('repo_name', 'group_name', 'challenge_won', 'challenge_won_timestamp', 'wasted_time', )
+    list_display = ('repo_name', 'group_name', 'challenge_won', 'challenge_won_timestamp', 'awaited_minutes', )
+
+    def awaited_minutes(self, obj):
+        seconds = obj.wasted_time / 1000
+        return str(timedelta(seconds=seconds))
+
 
 admin.site.register(SOGroup, SOGroupAdmin)
 admin.site.register(PingPong)
