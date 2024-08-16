02db92a8413a0fd1aec8ca22cbef5ff789856984
Javier Mansilla <javimansilla@gmail.com>
2024-08-16 00:33:17 -0300

    setting up logs

--- django_site/eeggs_site/settings.py
+++ django_site/eeggs_site/settings.py
@@ -164,3 +164,21 @@ STORAGES = {
     },
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'logs/debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
