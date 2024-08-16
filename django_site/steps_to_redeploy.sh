ec22b167a50460b0ddcd0199bcdb06abeda5cfdf
Javier Mansilla <javimansilla@gmail.com>
2024-08-16 00:58:01 -0300

    fixing bugs on shell script detected while using it

--- django_site/steps_to_redeploy.sh
+++ django_site/steps_to_redeploy.sh
@@ -4,11 +4,11 @@ git pull
 echo "pip install --upgrade pip"
 pip install --upgrade pip
 
-echo "pip install -r production-requirements.txt"
-pip install -r production-requirements.txt
+echo "pip install -r ../production-requirements.txt"
+pip install -r ../production-requirements.txt
 
-echo "pip install -r requirements.txt"
-pip install -r requirements.txt
+echo "pip install -r ../requirements.txt"
+pip install -r ../requirements.txt
 
 if ! test -f secrets.txt; then
   echo "File secrets.txt does not exist. Create it with the following content:"
@@ -22,9 +22,6 @@ python manage.py migrate
 
 echo "python manage.py collectstatic --noinput"
 python manage.py collectstatic --noinput
-
-echo "python manage.py runserver 0.0.0.0:8000"
-python manage.py runserver 0.0.0.0:8000
 
 EEGGS_PORT=2906
 echo "gunicorn eeggs_site.wsgi -b 127.0.0.1:${EEGGS_PORT}"
